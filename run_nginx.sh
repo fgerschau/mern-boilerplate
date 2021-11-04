c866a16d1d3225ff5fcbfb769a520c383a7b1b6e
Felix Gerschau <felix.gerschau@gmail.com>
2021-11-04 20:53:42 +0100

    Remove debugging command

--- run_nginx.sh
+++ run_nginx.sh
@@ -2,5 +2,4 @@
 # See https://serverfault.com/questions/577370/how-can-i-use-environment-variables-in-nginx-conf
 export DOLLAR='$'
 envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-cat /etc/nginx/nginx.conf
 nginx -g "daemon off;" -c /etc/nginx/nginx.conf
